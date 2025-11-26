80969ce468302af4d4676577be467d932074f9c3
Vishnevi <wewevilka@gmail.com>
2025-11-26 06:23:35 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log('Happy developing ✨')
+import express from 'express';
+import logger from "./logger.js";
+import parseResponse from "./parseResponse.js";
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+logger(app);
+parseResponse(app);
+
+app.get("/", (req, res) => {
+    res.send('Hello World!').status(200);
+})
+
+
+
+
+app.listen(PORT, () => console.log(`Listening at http://localhost:${PORT}`));

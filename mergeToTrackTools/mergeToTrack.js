415a23bcef2bcf92d56a121774a202484144d21a
Vishnevi <wewevilka@gmail.com>
2025-12-02 05:01:00 +0300

    add get tables data

--- mergeToTrackTools/mergeToTrack.js
+++ mergeToTrackTools/mergeToTrack.js
@@ -6,7 +6,27 @@ export async function mergeToTrack(inputSheetId, sheetIdTrack) { // ПРИНЯЛ
     const sheets = google.sheets({version: 'v4', auth: authClient});
 
     try {
-        console.log(`Input sheet id: ${inputSheetId}, new sheet id: ${sheetIdTrack}`);
+        const firstData = await sheets.spreadsheets.values.get({
+           spreadsheetId: inputSheetId,
+           range: 'Sheet1!A3:'
+        });
+
+        const rows = firstData.data.values;
+
+        // ↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓
+        //
+        // тут написать правила переноса данных из колонок
+        //
+        // ↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑
+
+        const existingRows = await sheets.spreadsheets.values.get({
+            spreadsheetId: sheetIdTrack,
+            range: 'Track!A3:R'
+        });
+
+        const currentRows = existingRows.data.values || [];
+
+
     } catch (err) {
         console.error(err);
     }

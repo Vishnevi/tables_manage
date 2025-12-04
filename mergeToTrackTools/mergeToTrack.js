dbb34db008534a4ab09545d96f2cb0d6b6037f33
Vishnevi <wewevilka@gmail.com>
2025-12-04 04:58:21 +0300

    add funcs for merge data to track

--- mergeToTrackTools/mergeToTrack.js
+++ mergeToTrackTools/mergeToTrack.js
@@ -8,24 +8,62 @@ export async function mergeToTrack(inputSheetId, sheetIdTrack) { // ПРИНЯЛ
     try {
         const firstData = await sheets.spreadsheets.values.get({
            spreadsheetId: inputSheetId,
-           range: 'Sheet1!A3:'
+           range: 'Sheet1!A3:CK'
         });
 
         const rows = firstData.data.values;
 
-        // ↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓
-        //
-        // тут написать правила переноса данных из колонок
-        //
-        // ↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑
+        const titleColumns = [9, 13, 17, 21, 25, 29, 33, 37, 41, 45, 49, 53, 57, 61, 65, 69, 73, 77, 81, 85];
 
-        const existingRows = await sheets.spreadsheets.values.get({
+        const artistsOutput = [];
+        const titlesOutput = [];
+        const ISRCOutput = [];
+
+
+       rows.forEach((row) => {
+           const artists = [4, 5, 6, 7, 8]
+               .map(i => row[i])
+               .filter(el => el && el.trim() !== '')
+               .join(', ');
+
+           titleColumns.forEach(i => {
+               const title = row[i];
+               const ISRC = row[i + 1]; // ISRC находится на следующем индексе от title
+
+               if (title && title.trim() !== '') {
+                   titlesOutput.push([title]);
+                   artistsOutput.push([artists])
+                   ISRCOutput.push([ISRC]);
+               }
+           });
+       });
+
+        await sheets.spreadsheets.values.update({
             spreadsheetId: sheetIdTrack,
-            range: 'Track!A3:R'
+            range: 'Track!B3',
+            valueInputOption: 'RAW',
+            requestBody: {
+                values: titlesOutput
+            }
         });
 
-        const currentRows = existingRows.data.values || [];
+        await sheets.spreadsheets.values.update({
+            spreadsheetId: sheetIdTrack,
+            range: 'Track!D3',
+            valueInputOption: 'RAW',
+            requestBody: {
+                values: artistsOutput
+            }
+        });
 
+        await sheets.spreadsheets.values.update({
+            spreadsheetId: sheetIdTrack,
+            range: 'Track!G3',
+            valueInputOption: 'RAW',
+            requestBody: {
+                values: ISRCOutput
+            }
+        });
 
     } catch (err) {
         console.error(err);
